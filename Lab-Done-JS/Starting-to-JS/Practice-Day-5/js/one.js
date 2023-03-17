6e92e6bfee1c01a58a56317f75cdc94769e32d00
hisu87 <numpadagain@gmail.com>
2023-03-17 08:00:13 +0700

    Update one.js

--- Lab-Done-JS/Starting-to-JS/Practice-Day-5/js/one.js
+++ Lab-Done-JS/Starting-to-JS/Practice-Day-5/js/one.js
@@ -1,11 +1,14 @@
-var $a = true; // change this value to test different cases
-
-if (typeof $a === "number") {
-  console.log("Variable $a has a value of number data type");
-} else if (typeof $a === "string") {
-  console.log("Variable $a has a value of string data type");
-} else if (typeof $a === "boolean") {
-  console.log("Variable $a has a value of boolean data type");
-} else{
-  console.log("Variable $a has a value of another data type");
+var a = prompt("Nhập giá trị cho biến a: ");
+if (!isNaN(a)) {
+  if (Number.isInteger(parseFloat(a))) {
+    console.log("Biến a có giá trị là dữ liệu kiểu số nguyên");
+  } else {
+    console.log("Biến a có giá trị là dữ liệu kiểu số thực");
+  }
+} else if (typeof a === 'string') {
+  console.log("Biến a có giá trị là dữ liệu kiểu chuỗi");
+} else if (typeof a === 'boolean') {
+  console.log("Biến a có giá trị là dữ liệu kiểu Boolean");
+} else {
+  console.log("Biến a có giá trị là dữ liệu kiểu khác");
 }
